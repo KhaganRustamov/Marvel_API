7ff45c669521f129a513db4d69c1f0f63f22f42d
Khagan <khaqan.rustamov@gmail.com>
2023-09-17 15:23:07 +0400

    update comicsList

--- src/components/charInfo/CharInfo.jsx
+++ src/components/charInfo/CharInfo.jsx
@@ -2,32 +2,20 @@ import { useState, useEffect } from "react";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 import "./charInfo.scss";
 
 const CharInfo = (props) => {
   const [char, setChar] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
     updateChar();
   }, [props.charId]);
 
-  const marvelService = new MarvelService();
+  const { loading, error, getCharacter, clearError } = useMarvelService();
 
   const onCharLoaded = (char) => {
-    setLoading(false);
     setChar(char);
-  };
-
-  const onCharLoading = () => {
-    setLoading(true);
-  };
-
-  const onError = () => {
-    setError(true);
-    setLoading(false);
   };
 
   const updateChar = () => {
@@ -35,8 +23,8 @@ const CharInfo = (props) => {
     if (!charId) {
       return;
     }
-    onCharLoading();
-    marvelService.getCharacter(charId).then(onCharLoaded).catch(onError);
+    clearError();
+    getCharacter(charId).then(onCharLoaded);
   };
 
   const skeleton = char || loading || error ? null : <Skeleton />;
--- src/hooks/http.hooks.js
+++ src/hooks/http.hooks.js
@@ -15,7 +15,7 @@ const useHttp = () => {
       try {
         const response = await fetch(url, { method, body, headers });
         if (!response.ok) {
-          throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+          throw new Error(`Could not fetch ${url}, status: ${response.status}`);
         }
         const data = await response.json();
         setLoading(false);
